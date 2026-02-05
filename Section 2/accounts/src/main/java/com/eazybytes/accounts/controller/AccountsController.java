b37c853063aa66f9afd9577ccea3f01ae11539e7
yara1611 <76455290+yara1611@users.noreply.github.com>
2026-02-06 01:20:30 +0200

    interacting with DB by setting up entity classes and repository interfaces

--- Section 2/accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ Section 2/accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -1,10 +1,12 @@
 package com.eazybytes.accounts.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccountsController {
 
+    @GetMapping("/sayHello")
     public String sayHello(){
         return "Hello World";
     }
